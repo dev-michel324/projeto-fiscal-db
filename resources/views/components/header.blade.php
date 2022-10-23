6bae5192ea6612621b4cf42625ce01687c9f46a1
dev-michel324 <dev_michel324@protonmail.com>
2022-10-23 14:48:09 -0300

    feat: added links to navbar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,18 +1,23 @@
-<nav class="navbar navbar-expand-lg bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">CUPOM FISCAL</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{ route('empresas.index') }}">Empresas</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{ route('instituicoes.index') }}">Instituição</a>
-        </li>
-      </ul>
-    </div>
+<nav class="navbar py-2 shadow-1 --bg-gradient-primary">
+  <div class="container display-flex align-items-center">
+      <div class="navbar-brand">
+        <a href="/">CUPOM FISCAL</a>
+      </div>
+      <div class="navbar-options">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ route('empresas.index') }}">Empresas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ route('instituicoes.index') }}">Instituição</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ route('produtos.index') }}">Produtos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ route('vendas.index') }}">Vendas</a>
+          </li>
+        </ul>
+      </div>
   </div>
 </nav>
