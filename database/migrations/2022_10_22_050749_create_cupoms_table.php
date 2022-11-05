533d38f60f204f12096fb9402a9d67c01599dab3
michel <dev_michel324@protonmail.com>
2022-11-05 20:39:46 -0300

    fix: added column foreign to institutions

--- database/migrations/2022_10_22_050749_create_cupoms_table.php
+++ database/migrations/2022_10_22_050749_create_cupoms_table.php
@@ -18,9 +18,13 @@ return new class extends Migration
             $table->timestamps();
             $table->time('hora');
             $table->boolean('doado')->default(0);
-            $table->bigInteger('empresa_id')->unsigned();
+            $table->bigInteger('empresa_id')->unsigned()->nullable();
             $table->foreign('empresa_id')->references('id')->on('empresas');
+        });
 
+        Schema::table('cupoms', function(Blueprint $table){
+            $table->bigInteger('instituicao_id')->unsigned()->nullable();
+            $table->foreign('instituicao_id')->references('id')->on('instituicaos');
         });
     }
 
