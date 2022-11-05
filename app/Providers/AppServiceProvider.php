b62ebcf10d9bf2fac93315924f63e048465ba487
michel <dev_michel324@protonmail.com>
2022-11-05 20:38:39 -0300

    feat: added authorization to donate coupon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Cupom;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('can_donate', function($cupom) {
+            if($cupom->doado)
+                return true;
+            return false;
+        });
     }
 }
